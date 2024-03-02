d3f5c2d4e33c7e1df8d4a91eecabc85a5a8b39df
NicolasCardenas <nicolas.cardenas.aroca@gmail.com>
2024-03-02 12:14:51 -0300

    Desafío IMC terminado

---
+++ imc.py
@@ -0,0 +1,21 @@
+
+peso_kg = float(input("Ingrese peso en kilogramos: "))
+altura_cm = float(input("Ingrese altura en centímetros: "))
+
+altura_mts = (altura_cm / 100)
+imc = (peso_kg/(altura_mts**2))
+
+print(f"Su IMC es {imc:.2f}")
+
+if imc<18.5 :
+    print("La clasificación OMS es Bajo Peso")
+elif 18.5<=imc<25:
+    print("La clasificación OMS es Adecuado")
+elif 25<=imc<30:
+    print("La clasificación OMS es Sobrepeso")
+elif 30<=imc<35:
+    print("La clasificación OMS es Obesidad Grado I")
+elif 35<=imc<40:
+    print("La clasificación OMS es Obesidad Grado II")
+elif imc>=40:
+    print("La clasificación OMS es Obesidad Grado III")
