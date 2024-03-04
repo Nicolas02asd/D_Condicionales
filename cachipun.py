cf78a5b3cc0a965f88165935c1ca5426a45fb262
NicolasCardenas <nicolas.cardenas.aroca@gmail.com>
2024-03-04 17:34:32 -0300

    Se termina juego y se añade filtro a imc

--- cachipun.py
+++ cachipun.py
@@ -2,4 +2,23 @@ import random
 
 mylist = ["piedra", "papel", "tijeras"]
 
-print(random.choice(mylist))
+computadora = random.choice(mylist)
+cachipun = input("Tu jugada es ")
+
+if cachipun not in mylist:
+    print("Argumento inválido: Debe ser piedra, papel o tijera.") 
+else:  
+    print(f"Computadora jugo {computadora}") 
+    
+# Se desplazo el código para evitar que se imprima "ganaste" cuando se ingresa mal la jugada.
+    if computadora == cachipun:
+        print("Es un empate, sigue intentando.")
+    elif (computadora=="tijeras")and(cachipun=="papel"):
+        print("Gana la computadora, suerte la próxima.")
+    elif (computadora=="piedra")and(cachipun=="tijeras"):
+        print("Gana la computadora, suerte la próxima.")
+    elif (computadora=="papel")and(cachipun=="piedra"):
+        print("Gana la computadora, suerte la próxima.")     
+    else:
+        print("Ganaste!!")   
+
--- imc.py
+++ imc.py
@@ -5,17 +5,21 @@ altura_cm = float(input("Ingrese altura en centímetros: "))
 altura_mts = (altura_cm/100)
 imc = (peso_kg/(altura_mts**2))
 
-print(f"Su IMC es {imc:.2f}")
+# Filtro para números negativos o cero.
+if imc <= 0:
+    print("Ingrese datos validos")
+else:
+    print(f"Su IMC es {imc:.2f}")
 
-if imc<18.5:
-    print("La clasificación OMS es Bajo Peso")
-elif 18.5<=imc<25:
-    print("La clasificación OMS es Adecuado")
-elif 25<=imc<30:
-    print("La clasificación OMS es Sobrepeso")
-elif 30<=imc<35:
-    print("La clasificación OMS es Obesidad Grado I")
-elif 35<=imc<40:
-    print("La clasificación OMS es Obesidad Grado II")
-elif imc>=40:
-    print("La clasificación OMS es Obesidad Grado III")
+    if imc<18.5:
+        print("La clasificación OMS es Bajo Peso")
+    elif 18.5<=imc<25:
+        print("La clasificación OMS es Adecuado")
+    elif 25<=imc<30:
+        print("La clasificación OMS es Sobrepeso")
+    elif 30<=imc<35:
+        print("La clasificación OMS es Obesidad Grado I")
+    elif 35<=imc<40:
+        print("La clasificación OMS es Obesidad Grado II")
+    elif imc>=40:
+        print("La clasificación OMS es Obesidad Grado III")
